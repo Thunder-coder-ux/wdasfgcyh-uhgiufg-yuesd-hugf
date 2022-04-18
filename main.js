8032287a88efab4edc718106234b76ab3d31f057
Ninja_joeneys <81759049+Thunder-coder-ux@users.noreply.github.com>
2022-04-18 17:41:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('Mobile Net', modelLoaded);
+}
+
+function modelLoaded(){
+  console.log("MobileNet loaded");
+}
+
+function draw(){
+  image(video, 0, 0, 300, 300);
+  classifier.classify(video, gotResult);
+}
+var previous_result = '';
+
+function gotResult(error, result){
+  if(error){
+    console.error(error);
+  }
+  else{
+    if((result[0].confidence > 0.5) && (previous_result != result[0].label)){
+      console.log(result);
+      previous_result = result[0].label;
+      var synth = window.speechSynthesis;
+      speak_data = "Object detected is " + result[0].label;
+      var utterThis = new SpeechSynthesisUtterance(speak_data);
+      synth.speak(utterThis);
+
+      document.getElementById("result_object_name").innerHTML = result[0].label;
+      document.getElementById("result_object_accuracy").innerHTML = Math.round(results[0].confidence * 100) + "%";
+    }
+  }
+}
+
